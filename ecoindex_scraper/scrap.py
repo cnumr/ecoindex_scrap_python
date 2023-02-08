2f8c77f782358dd2f16e14c67002827b3c582dfa
Édouard Lopez <edouard-lopez@users.noreply.github.com>
2023-02-08 15:36:36 +0100

    fix: cnumr/ecoindex_cli#226

--- ecoindex_scraper/scrap.py
+++ ecoindex_scraper/scrap.py
@@ -5,7 +5,7 @@ from time import sleep
 from typing import Dict, List, Tuple
 from warnings import filterwarnings
 
-import undetected_chromedriver.v2 as uc
+import d as uc
 from ecoindex.ecoindex import get_ecoindex
 from ecoindex.models import PageMetrics, PageType, Result, ScreenShot, WindowSize
 from pydantic.networks import HttpUrl
